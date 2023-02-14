cc32ddf54ef13805e81035ef25f9b0d5ac1fec31
jordan <jordan21398@gmail.com>
2023-02-13 20:45:01 -0500

    Add random computer player

--- src/console_player.py
+++ src/console_player.py
@@ -2,11 +2,15 @@ import json
 import logging
 import sys
 from abc import ABC, abstractmethod
+from itertools import combinations
+from random import randint
+from typing import Optional
 
 import zmq
 
 from src.constants import TABLE_STACKS
-from src.game_types import Action, Response, Update, UpdateType
+from src.game_types import Action, Response, Stack, Update, UpdateType
+from src.utilities import deserialize_cards, get_available_plays_from_stack
 
 logging.basicConfig(level=logging.INFO)
 
@@ -19,6 +23,8 @@ class Player(ABC):
         self.table_stacks = TABLE_STACKS
         self.unaccepted_play_cards: str
         self.player_number = player_number
+        self.last_play: Optional[Stack] = None
+        self.discard_pile = Stack()
 
     def bind_to_socket(
         self,
@@ -57,6 +63,16 @@ class Player(ABC):
                 assert update.cards
                 for card in update.cards.split(","):
                     self.hand_set.add(card)
+                self.last_play = None
+                self.discard_pile.empty()
+            case UpdateType.PLAYER_PICKED_UP_DISCARD_PILE | UpdateType.BURN_DISCARD_PILE:
+                self.last_play = None
+                self.discard_pile.empty()
+            case UpdateType.PLAY_FROM_HAND | UpdateType.PLAY_FROM_TABLE | UpdateType.PLAY_FROM_FACEDOWN_SUCCESS:
+                assert update.cards
+                deserialized_cards = deserialize_cards(encoded_cards=update.cards)
+                self.last_play = deserialized_cards
+                self.discard_pile += deserialized_cards
             case _:
                 pass
 
@@ -133,7 +149,59 @@ class HumanPlayer(Player):
 
 class ComputerPlayer(Player):
     def handle_request(self, message_dict: dict) -> None:
-        raise NotImplementedError("ComputerPlayer is not implemented yet")
+
+        logging.info("Request received")
+
+        if message_dict["request_type"] == "SET_TABLE_CARDS":
+            encoded_cards = self.set_table_cards()
+            response = Response(action=Action.SET_TABLE_CARDS, cards=encoded_cards)
+            self.unaccepted_play_cards = encoded_cards
+        elif message_dict["request_type"] == "PLAY":
+            play = self.play()
+            if play == "1":
+                response = Response(action=Action.PICK_UP_DISCARD_PILE)
+            else:
+                response = Response(action=Action.PLAY_KNOWN_CARDS, cards=play)
+                self.unaccepted_play_cards = play
+
+        self.socket.send_string(response.json())
+
+        logging.info("Response sent")
+
+    @abstractmethod
+    def set_table_cards(self) -> str:
+        raise NotImplementedError("Method not implemented")
+
+    @abstractmethod
+    def play(self) -> str:
+        raise NotImplementedError("Method not implemented")
+
+
+class RandomPlayer(ComputerPlayer):
+    def set_table_cards(self) -> str:
+        card_combinations = [
+            ",".join(combination) for combination in combinations(self.hand_set, TABLE_STACKS)
+        ]
+        chosen_combination = pick_one(options=card_combinations)
+        return chosen_combination
+
+    def play(self) -> str:
+        options = []
+        if self.last_play is not None:
+            options.append("1")
+        available_cards_set = self.hand_set if len(self.hand_set) else self.table_set
+        available_cards_encoded = ",".join(available_cards_set)
+        available_cards = deserialize_cards(encoded_cards=available_cards_encoded)
+        available_plays = get_available_plays_from_stack(
+            stack=available_cards, last_play=self.last_play, discard_pile=self.discard_pile
+        )
+        options += list(available_plays)
+        return pick_one(options=options)
+
+
+def pick_one(options: list[str]) -> str:
+    chosen_index = randint(0, len(options) - 1)
+    return options[chosen_index]
 
 
 def print_update(update: Update) -> None:
@@ -156,7 +224,15 @@ def print_update(update: Update) -> None:
 if __name__ == "__main__":
 
     player_number = int(sys.argv[1])
+    player_type = sys.argv[2]
 
-    player = HumanPlayer(player_number=player_number)
+    player_types = {
+        "human": HumanPlayer,
+        "random": RandomPlayer,
+    }
+
+    player_class = player_types[player_type]
+
+    player = player_class(player_number=player_number)
     player.bind_to_socket()
     player.run_communication_loop()
